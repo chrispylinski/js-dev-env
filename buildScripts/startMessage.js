66c8ce648ecc060d0c0417baec2d747a64752f21
tacoder <chris.pylinski@googlemail.com>
2020-02-23 20:39:11 +0100

    added .babelrc, .editconfig, srcServer.js, startMessage.js, confiugred package.json, added index.css, index.html, index.js, and the webpack.config.dev.js file

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+// ES5 way: var chalk = require('chalk');
+import chalk from "chalk"; // ES6 code in the way to import library to specify the output color
+
+console.log(chalk.green("Starting app in dev mode..."));
---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+import path from "path";
+
+export default {
+  debug: true, // enables debug information
+  devtool: "inline-source-map", // many devtools trade-off quality vs. time/speed
+  noInfo: false, // webpack will list of files its bundling, usually off==false bc it has a lot of noise
+  entry: [path.resolve(__dirname, "src/index")], // THIS IS THE APP ENTRY POINT
+  target: "web", // we target the web or Node if Node needs to work with it instead of the web
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [], // e.g. hot reloading
+  module: {
+    // telling webpack what files to consider
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] }, // handle JS files
+      { test: /\.css$/, loaders: ["style", "css"] } // handle CSS files
+    ]
+  }
+};
