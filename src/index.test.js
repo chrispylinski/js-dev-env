bd328cfca6c8b328660252ed45c0d2c8c0c343d1
tacoder <chris.pylinski@googlemail.com>
2020-04-25 20:48:02 +0200

    added inline comments for the testing code

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import jsdom from "jsdom";
-import fs from "fs";
+import fs from "fs"; // filesystem using node
 
 describe("Our first test", () => {
   it("should pass", () => {
@@ -9,14 +9,18 @@ describe("Our first test", () => {
 });
 
 describe("index.html", () => {
-  it("should say hello", done => {
+  it("should say hello", (done) => {
+    // async call thats why done parameter is needed here
+    // get reference to index.html file
     const index = fs.readFileSync("./src/index.html", "utf-8");
-    jsdom.env(index, function(err, window) {
+    // now content in-memory within a constant called index
+    // callback function / window is just like browser window
+    jsdom.env(index, function (err, window) {
       // second parameter is for any JS files
       const h1 = window.document.getElementsByTagName("h1")[0];
       expect(h1.innerHTML).to.equal("Hello World!");
       done(); // tell Mocha here that our Test is done.
-      window.close();
+      window.close(); // to free up memory when we created DOM
     });
   });
 });
